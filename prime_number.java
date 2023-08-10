013f45269118868df744e0a731e46aa95a0e5b24
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-08-11 01:25:19 +0530

    base concept

---
+++ prime_number.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class prime_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the no ");
+        int num =sc.nextInt();
+        int count=0;
+        if(num==0||num==1)
+        {
+            System.out.println("the number is not prime"+num);
+        }
+        if(num>1)
+        {
+            for(int i=1;i<=num;i++)
+            {
+                if(num%i==0)
+                count++;
+            }
+                if(count==2)
+                {
+                    System.out.println("the number is prime"+num);
+                }
+                else
+                {
+                    System.out.println("the number is  not prime"+num);
+                }
+        }
+    }
+}
