9f8779ad7519e31c25f7b8429179f86505410457
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-08-26 11:47:00 +0530

    basic

---
+++ reverse_integer.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class reverse_integer {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("enter the number");
+       int n =sc.nextInt();
+        int rem;
+        int rev=0;
+       while(n>0)
+       {
+        rem =n%10;
+        rev= (rev*10)+rem;
+        n= n/10;
+       }
+       System.out.println("the reversed no we get");
+       System.out.println(rev);
+    }
+}
