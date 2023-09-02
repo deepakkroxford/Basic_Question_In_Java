7df86392fce3420175bf147209aee466d79ac630
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-09-03 01:05:09 +0530

    important

---
+++ Leading_Zero_number.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Leading_Zero_number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value n1");
+        int n1 = sc.nextInt();
+        System.out.println("enter the value n2");
+        int n2 = sc.nextInt();
+
+        for(int i=n1;i<=n2;i++)
+        {
+            if(n2>=100)
+            {
+                String formattedStr = String.format("%03d", i);
+                System.out.println(formattedStr);
+            }
+            else if(n2>=10)
+            {
+                String formattedStr = String.format("%02d", i); 
+            System.out.println(formattedStr);           }
+            else
+            {
+                String formattedStr = String.format("%d", i);
+                System.out.println(formattedStr);
+            }
+        }
+    }
+}
