0836ee637318c0c9aff16bc4d847e160f20f91e6
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-09-03 13:12:59 +0530

    Update Leading_Zero_number.java

--- Leading_Zero_number.java
+++ Leading_Zero_number.java
@@ -8,19 +8,14 @@ public class Leading_Zero_number {
         System.out.println("enter the value n2");
         int n2 = sc.nextInt();
 
-        for(int i=n1;i<=n2;i++)
-        {
-            if(n2>=100)
-            {
+        for (int i = n1; i <= n2; i++) {
+            if (n2 >= 100) {
                 String formattedStr = String.format("%03d", i);
                 System.out.println(formattedStr);
-            }
-            else if(n2>=10)
-            {
-                String formattedStr = String.format("%02d", i); 
-            System.out.println(formattedStr);           }
-            else
-            {
+            } else if (n2 >= 10) {
+                String formattedStr = String.format("%02d", i);
+                System.out.println(formattedStr);
+            } else {
                 String formattedStr = String.format("%d", i);
                 System.out.println(formattedStr);
             }
