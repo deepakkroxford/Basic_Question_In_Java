e9bcf99cc701602cf403f71121f3d4786944a635
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-09-04 19:52:04 +0530

    .

--- Fibnacci_series.java
+++ Fibnacci_series.java
@@ -1,23 +1,20 @@
 import java.util.*;
+
 public class Fibnacci_series {
-    public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-       System.out.println("enter the number");
-       int n = sc.nextInt();
-       
-       if(n==0)
-       {
-        System.out.println(n);
-       }
-       int a=0;
-       int b =1;
-       int c=0;
-       for(int i =2;i<=n;i++)
-       {
-          c =a+b;
-          a=b;
-          b=c;
-       }
-       System.out.println(c);
-    }
+   public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter the number");
+      int n = sc.nextInt();
+      int a = 0;
+      int b = 1;
+      int c = 0;
+      for (int i = 2; i <= n; i++) {
+         System.out.println(a);
+         c = a + b;
+         a = b;
+         b = c;
+
+      }
+      System.out.println("the fibnacii of the number is "+c);
+   }
 }
