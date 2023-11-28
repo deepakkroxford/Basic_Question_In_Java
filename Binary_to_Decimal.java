d2ffbc39fe24ec13a133b0f713ca2594e6732a7e
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-11-28 12:08:53 +0530

    basic

---
+++ Binary_to_Decimal.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Binary_to_Decimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the binary number");
+        int n = sc.nextInt();
+        int ans =0; int i=0;
+        while(n>0)
+        {
+            int rem = n%10;
+            ans = ans+ rem * (int)Math.pow(2, i++);
+            n=n/10;
+
+        }
+        System.out.println("the decimal number be" +ans);
+    }
+}
---
+++ Decimal_to_Binaray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Decimal_to_Binaray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the value of n");
+        int n = sc.nextInt();
+        
+       
+        int ans=0;
+        int palacevalue = 1;
+        while (n>0) {
+            int rem =n%2;
+            ans= ans + rem * palacevalue;
+            n=n/2;
+            palacevalue=palacevalue*10;
+
+        }
+        System.out.println(ans);
+    }
+}
