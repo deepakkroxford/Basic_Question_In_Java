10dde5eea8a3418389e79b3281fd1797067d9458
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2023-11-29 22:01:55 +0530

    Update Decimal_to_Binaray.java

--- Decimal_to_Binaray.java
+++ Decimal_to_Binaray.java
@@ -3,10 +3,9 @@ import java.util.Scanner;
 public class Decimal_to_Binaray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the value of n");
+        System.out.println("enter the decimal number");
         int n = sc.nextInt();
-        
-       
+
         int ans=0;
         int palacevalue = 1;
         while (n>0) {
