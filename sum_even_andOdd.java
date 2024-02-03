338591d37f5c349b7fe59318fea640507df51cce
Deepak Kumar Singh <93421333+deepakkroxford@users.noreply.github.com>
2024-02-03 10:37:24 +0530

    important basic question

---
+++ sum_even_andOdd.java
@@ -0,0 +1,26 @@
+public class sum_even_andOdd {
+    public static void main(String[] args) {
+        int num =552245;
+        int evenSum=0;
+        int oddSum=0;
+        int arr[]=new int [2];
+		  int rem=0;
+          int sum=0;
+		 while(num>0)
+		 {
+			 rem=num%10;
+			 num= num/10;
+			 if(rem%2==0)
+			 {
+				 evenSum=evenSum+rem;
+                 
+			 }
+			 else{
+				 oddSum=oddSum+rem;
+                 
+			 }
+		 }
+		 System.out.println(evenSum);
+         System.out.println(oddSum);
+    }
+}
